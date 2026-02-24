83dfc7dbb9229ba6070f0221a2cd2cc2c0128664
HimanshuGupta08 <himanshu.it456@gmail.com>
2026-02-24 16:43:51 +0530

    updates

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbzM0pJvWyuITALDI9Ef5HD3vnlheHzoAKFLuY-l1hAOaWAfq3jg4fznPxD7LHjOJG0W/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbwfWnBGgiFeOKyGFcZW7x0Yvi60GXLn38iRnYqEGPnTe_0BtIxZTzkYdlbeFRNE-CGj/exec";
 
 const form = document.getElementById("leadForm");
 const responseMessage = document.getElementById("responseMessage");
